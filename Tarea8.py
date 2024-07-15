771c2a614524c5998190964d76ed57df898cf9f1
Emiirck <emirck.candelo@tecnologicoliceocristiano.edu.ec>
2024-07-15 18:31:06 -0500

    Tarea8

---
+++ Tarea8.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese el primer número entero: "))
+numero2 = int(input("Ingrese el segundo número entero: "))
+if numero1 < numero2:
+    menor = numero1
+else:
+    menor = numero2
+print("El menor es:", menor)
