9a7c4f357b85ce765bb81085cbd6e430e1775d5c
Emiirck <emirck.candelo@tecnologicoliceocristiano.edu.ec>
2024-07-15 17:48:45 -0500

    Tarea2

---
+++ Tarea2.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Ingrese un número: "))
+numero2 = float(input("Ingrese otro número: "))
+suma = numero1 + numero2
+print("Suman:", suma)
+nuevo_numero = float(input("Ingrese un nuevo número: "))
+multiplicacion = suma * nuevo_numero
+print("La Multiplicación de la suma por el último número es:", multiplicacion)
