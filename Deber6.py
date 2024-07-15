27c6448dbb9a56c153998018c21f2928c601682d
Emiirck <emirck.candelo@tecnologicoliceocristiano.edu.ec>
2024-07-15 18:17:39 -0500

    Tarea6

---
+++ Deber6.py
@@ -0,0 +1,4 @@
+edad = int(input("Ingrese la edad de la persona: "))
+articulos_comprados = int(input("Ingrese la cantidad de artículos comprados: "))
+resultado = edad > 18 and articulos_comprados > 1
+print(resultado)
